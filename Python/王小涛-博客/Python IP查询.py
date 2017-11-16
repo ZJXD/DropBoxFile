b3e874fea2ac308787e0f19b95840e16e0c0a566
朱海涛 <ZHT@DESKTOP-05R7QAA>
2017-11-16 22:00:59 +0800

    同步3

--- Python/王小涛-博客/Python IP查询.py
+++ Python/王小涛-博客/Python IP查询.py
@@ -1,10 +1,10 @@
 import urllib.request as requests
 import json
 
-ip = '101.33.128.216'
+ip = '221.2.2.15'
 key = '9cb9a1624edd4ae668d54ebbd7361628'
 
-url = "http://a.apix.cn/tongyu/iplookup/ip?ip=" + ip
+url = "http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=js&ip=" + ip
 
 headers = {
     'accept': "application/json",  
@@ -14,7 +14,8 @@ headers = {
 
 req = requests.Request(url,headers = headers)
 res = requests.urlopen(req).read().decode()
-s = json.loads(res)['data']
+res = res[20:len(res)-1]
+s = json.loads(res)
 
 print (s)
-print (s['country'] + ' ' + s['province'] + '省' + s['city'] + '市 ' + s['operator'])
+print (s['country'] + ' ' + s['province'] + '省 ' + s['city'] + '市 ' + s['district'])
