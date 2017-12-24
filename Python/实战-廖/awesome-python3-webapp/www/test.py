6859127e8163543135885417a49e1b304ec21efd
朱海涛 <ZHT@DESKTOP-05R7QAA>
2017-12-24 20:45:14 +0800

    同步13

--- Python/实战-廖/awesome-python3-webapp/www/test.py
+++ Python/实战-廖/awesome-python3-webapp/www/test.py
@@ -7,12 +7,12 @@ from Models import User, Blog, Comment
 
 @asyncio.coroutine
 def test(loop):
-    yield from orms.create_pool(loop = loop,user='masql', password='z456', db='test')
+    yield from orms.create_pool(loop = loop,user='root', password='zhtding', db='test')
 
     # u = User(name='Test', email='zht@example.com', passwd='1234567890', image='about:blank')
     
     # yield from u.save()
-    b = Blog(user_id = '001513130976719dd233989e24e463fb1bf6449f36e85a9000',user_name='Test',user_image = 'about:blank',name = 'TestBlog5',summary = 'TestBlog5 from database',content = 'TestBlog5 from database,and so on.....')
+    b = Blog(user_id = '00151411127828430221c21d76246cda9a8b643a394e6c1000',user_name='Test',user_image = 'about:blank',name = 'TestBlog3',summary = 'TestBlog3 from database',content = 'TestBlog3 from database,and so on.....')
     yield from b.save()
 
 loop = asyncio.get_event_loop()
